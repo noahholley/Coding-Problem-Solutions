6218bd27b8f981cf696089403578115d7f7f83f2
Noah holley <noahholley@users.noreply.github.com>
2017-09-20 09:35:53 -0600

    Create grading_students.py

---
+++ grading_students.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+def solve(grades):
+    # Complete this function
+
+n = int(input().strip())
+grades = []
+grades_i = 0
+for grades_i in range(n):
+   grades_t = int(input().strip())
+   grades.append(grades_t)
+result = solve(grades)
+print ("\n".join(map(str, result)))
